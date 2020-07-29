8348322770393435ad6be7c31ca69a224d2b5545
Joel Borges Junior <joelborgesjunior@hotmail.com>
2020-07-29 12:15:43 -0300

    Primeira conexão entre Bot/DialogFlow/C#

---
+++ DialogFlow.cs
@@ -0,0 +1,48 @@
+using System;
+using Google.Cloud.Dialogflow.V2;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Telegram.Bot.Args;
+using System.IO;
+
+namespace DioCarrefourBot
+{
+    public class DialogFlow
+    {   
+        public static async void sendMessage(object sender, MessageEventArgs e)
+        {
+            //Instanciando um objeto para abrir a sessão
+            SessionClient sc = new SessionClient();
+            //Dando o nome do arquivo onde está as informações para conexão com o DialogFlow
+            dynamic filePath = File.ReadAllText("DioCarrefourBot.json");
+                       
+            dynamic jsonTratado = JsonSerializer.Deserialize<SessionClient>(filePath);
+
+            //Atribuindo valores do Parse as variáveis do objeto
+            sc.client_email = jsonTratado.client_email;                
+            sc.private_key = jsonTratado.private_key;
+            sc.project_id = jsonTratado.project_id;  
+
+            SessionsClient client = await SessionsClient.CreateAsync();
+
+            DetectIntentRequest request = new DetectIntentRequest 
+            {
+                SessionAsSessionName = SessionName.FromProjectSession(sc.project_id, e.Message.Chat.Id.ToString()),
+                QueryInput = new QueryInput()
+                {
+                    Text = new TextInput()
+                    {
+                        Text = e.Message.Text,
+                        LanguageCode = "pt-BR",
+                    }
+                }
+            };           
+            DetectIntentResponse response = await client.DetectIntentAsync(request);
+
+            await Program.botClient.SendTextMessageAsync(
+                chatId: e.Message.Chat.Id,
+                text: response.QueryResult.FulfillmentText
+            );
+        }
+    }        
+}
---
+++ SessionClient.cs
@@ -0,0 +1,13 @@
+namespace DioCarrefourBot
+{
+    public class SessionClient
+    {     
+        public string project_id { get; set; }
+        public string private_key { get; set; }
+        public string client_email { get; set; }   
+           
+        public SessionClient()
+        {            
+        }
+    }
+}
